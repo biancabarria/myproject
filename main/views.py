26b8b81cb8149011479834bf57b33deedfe41d60
Bianca Barria <bncbarria@gmail.com>
2022-02-20 23:49:48 -0300

    fix: form rendering

--- main/views.py
+++ main/views.py
@@ -5,6 +5,6 @@ def home(response):
     return render(response, "main/home.html", {})
 
 def form(response):
-    return render(response, "main/home.html", {})
+    return render(response, "main/form.html", {})
 
 # Create your views here.
