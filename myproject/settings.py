2ff740d11b1f59a6cfee4b154d5c50a63c6378ca
Bianca Barria <bncbarria@gmail.com>
2022-02-20 22:53:38 -0300

    feat: adding django-heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+
+import django
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,3 +127,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.setting(locals())
