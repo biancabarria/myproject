9be2dd1f1f85664f07be64b171c4d46310ade7df
Bianca Barria <bncbarria@gmail.com>
2022-02-20 23:33:32 -0300

    feat: adding logic for register and home

--- register/views.py
+++ register/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import login, authenticate
+from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
@@ -8,7 +9,12 @@ def register(response):
     if response.method == "POST":
         form = UserCreationForm(response.POST)
         if form.is_valid():
-            form.save()
+            new_user = form.save()
+            messages.info(response, "Thanks for registering. You are now logged in.")
+            new_user = authenticate(username=form.cleaned_data['username'],
+                password=form.cleaned_data['password'],
+                )
+            login(response, new_user)
         return redirect("/")
     else:
         form = UserCreationForm()
